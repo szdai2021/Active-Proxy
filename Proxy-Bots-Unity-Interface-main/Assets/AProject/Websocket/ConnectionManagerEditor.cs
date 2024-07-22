018daf3c7e49be99c2aa5bfe3798124dd7a44d3d
shaozhang <dai.shaozhang@gmail.com>
2024-07-22 16:50:50 +1000

    22-07-2024

---
+++ Proxy-Bots-Unity-Interface-main/Assets/AProject/Websocket/ConnectionManagerEditor.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using UnityEditor;
+
+[CustomEditor(typeof(ConnectionManager))]
+public class ConnectionManagerEditor : Editor
+{
+    public string ipAddress = "ws://localhost:8080";
+
+    private WebSocket ws;
+
+    public Dictionary<int, Vector2> posReceived = new Dictionary<int, Vector2>();
+    public Dictionary<int, float> rotReceived = new Dictionary<int, float>();
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        ConnectionManager myScript = (ConnectionManager)target;
+
+        if (GUILayout.Button("re-connect"))
+        {
+            myScript.Connect();
+        }
+    }
+}
